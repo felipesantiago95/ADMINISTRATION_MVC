69b73c5a0ef214612a31cca48b441300f7eb49f5
felipesantiago95 <felipe.palan@udla.edu.ec>
2024-05-14 23:39:51 -0500

    upload first

---
+++ Controllers/AccesController.cs
@@ -0,0 +1,65 @@
+﻿using LOGIN_ADMINISTRATION.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+using LOGIN_ADMINISTRATION.Data;
+
+namespace LOGIN_ADMINISTRATION.Controllers
+{
+    public class AccesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario _usuario)
+        {
+            DA_logica _da_usuario = new DA_logica();
+
+            var usuario = _da_usuario.ValidarUsuario(_usuario.Correo, _usuario.Clave);
+
+            if (usuario != null)
+            {
+                //2.- CONFIGURACION DE LA AUTENTICACION
+                #region AUTENTICACTION
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, usuario.Nombre),
+                    new Claim("Correo", usuario.Correo),
+                };
+                foreach (string rol in usuario.Roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, rol));
+                }
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                #endregion
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public async Task<IActionResult> Salir()
+        {
+            //3.- CONFIGURACION DE LA AUTENTICACION
+            #region AUTENTICACTION
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            #endregion
+
+            return RedirectToAction("Index");
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/Acces/Index";
+        option.ExpireTimeSpan= TimeSpan.FromMinutes(20);
+        option.AccessDeniedPath = "/home/Privacy";
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Acces}/{action=Index}/{id?}");
+
+app.Run();
